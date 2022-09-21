9b4d189b916173b4cbe5cb01057c86c40ecad1cd
dindapertiwi <113920687+dindapertiwi@users.noreply.github.com>
2022-09-21 14:01:14 +0700

    hello world

---
+++ hello.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	// belajar hello
+	//fmt.Println("Hello")
+	fmt.Println("Dinda", "Ayu", "Hikmah", "Pertiwi", "Belajar Ngoding")
+
+	//
+	fmt.Println("Hello")
+	fmt.Println("Hello", "Dinda")
+
+	fmt.Print("Hello Dinda\n")
+	fmt.Print("Hello", " Dinda\n")
+	fmt.Print("Hello", " ", "Dinda\n")
+}
